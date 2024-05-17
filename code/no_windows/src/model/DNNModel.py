bf22423996492ec48b1077963965aa7baf90b432
Cedric Aebi <cedric.aebi@me.com>
2024-05-17 18:31:29 +0200

    adding checkpoint

---
+++ code/no_windows/src/model/DNNModel.py
@@ -0,0 +1,65 @@
+import uuid
+
+import numpy as np
+import keras
+from imblearn.base import BaseSampler
+from keras import layers
+import tensorflow as tf
+from scikeras.wrappers import KerasClassifier
+from sklearn.base import BaseEstimator
+
+from model.AbstractModel import AbstractModel
+
+
+class DNNModel(AbstractModel):
+    def __init__(self, scaler: BaseEstimator, resampler: BaseSampler, number_of_features: int, run_info: dict):
+        tf.random.set_seed(42)
+        keras.utils.set_random_seed(42)
+        build_model = self._build_model(number_of_features=number_of_features)
+        early_stopping_callback = keras.callbacks.EarlyStopping(monitor="loss", patience=9)
+        self._unique_run_id = str(uuid.uuid4())
+        log_dir = "../logs/fit/" + self._unique_run_id
+        run_info["log_dir"] = log_dir
+        tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
+        model = KerasClassifier(
+            model=build_model,
+            epochs=500,
+            batch_size=32,
+            verbose=1,
+            validation_split=0.2,
+            callbacks=[early_stopping_callback, tensorboard_callback],
+        )
+        super().__init__(model=model, scaler=scaler, resampler=resampler)
+
+    def fit(self, x_train: np.ndarray, y_train: np.ndarray, grid_search: bool, run_info: dict) -> None:
+        self._pipeline.fit(x_train, y_train)
+
+    def predict(self, x: np.ndarray) -> np.ndarray:
+        return self._pipeline.predict(x)
+
+    @staticmethod
+    def _build_model(number_of_features: int) -> keras.Sequential:
+        # Define the model
+        # For the non-window approach use a simpler model
+        if number_of_features == 1:
+            model = keras.Sequential()
+            model.add(layers.Dense(128, input_dim=number_of_features, activation="relu"))
+            model.add(layers.Dense(64, activation="relu"))
+            model.add(layers.Dense(32, activation="relu"))
+            # Output layer with 1 neuron, sigmoid activation for binary classification
+            model.add(layers.Dense(1, activation="sigmoid"))
+        else:
+            model = keras.Sequential()
+            model.add(layers.Dense(512, input_dim=number_of_features, activation="relu"))
+            model.add(layers.Dense(256, activation="relu"))
+            model.add(layers.Dense(128, activation="relu"))
+            model.add(layers.Dense(64, activation="relu"))
+            model.add(layers.Dense(54, activation="relu"))
+            model.add(layers.Dense(50, activation="relu"))
+            # Output layer with 1 neuron, sigmoid activation for binary classification
+            model.add(layers.Dense(1, activation="sigmoid"))
+
+        # Compile the model
+        model.compile(loss="binary_crossentropy", optimizer="adam", metrics=["accuracy"])
+
+        return model

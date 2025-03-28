b76984da5d7d361c2b28a44a1d605eb187389770
Cedric Aebi <cedric.aebi@me.com>
2025-03-28 15:10:19 +0100

    changed some stuff

--- src/model/XGBoostModel.py
+++ src/model/XGBoostModel.py
@@ -29,7 +29,7 @@ class XGBoostModel(AbstractModel):
 
     def fit(self, x_train: pd.DataFrame, y_train: pd.DataFrame, run_info: dict) -> None:
         self._grid_search_cv = GridSearchCV(
-            estimator=self._pipeline, param_grid=self._hyperparameter_grid, cv=self._cv, n_jobs=6, verbose=2
+            estimator=self._pipeline, param_grid=self._hyperparameter_grid, cv=self._cv, n_jobs=1, verbose=2
         )
         self._grid_search_cv.fit(x_train, y_train)
         self._best_estimator = self._grid_search_cv.best_estimator_

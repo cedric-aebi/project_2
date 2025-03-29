22c8bdecc580126b555462176e3dbc87add39ef0
Cedric Aebi <cedric.aebi@me.com>
2025-03-29 11:52:51 +0100

    changed batch sizes

--- src/model/ShallowNNModel.py
+++ src/model/ShallowNNModel.py
@@ -56,7 +56,7 @@ class ShallowNNModel(AbstractModel):
             estimator=self._pipeline,
             param_grid=self._hyperparameter_grid,
             cv=self._cv,
-            n_jobs=8 if self._dataset == Dataset.STRESS else 4,
+            n_jobs=2,
             verbose=2,
         )
         self._grid_search_cv.fit(x_train, y_train)

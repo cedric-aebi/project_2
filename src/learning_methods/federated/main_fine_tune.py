850da5082712fc46bbf8783f0210297a6f6ec171
Cedric Aebi <cedric.aebi@me.com>
2025-06-21 14:15:47 +0200

    updated fine tuning script

--- src/learning_methods/federated/main_fine_tune.py
+++ src/learning_methods/federated/main_fine_tune.py
@@ -1,23 +1,17 @@
 from pathlib import Path
 
-from enums.ResamplingMethod import ResamplingMethod
-from enums.ScalingMethod import ScalingMethod
+from service.argumentservice.ArgumentService import ArgumentService
 from service.exportservice.ExportService import ExportService
 from enums.Model import Model
-from sklearn.model_selection import train_test_split
 import joblib
 from utils import utils
 from enums.Dataset import Dataset
-from sklearn.preprocessing import StandardScaler
 from keras.src.optimizers import Adam
 from sklearn.metrics import confusion_matrix, accuracy_score, recall_score, precision_score, f1_score
 import pandas as pd
 import xgboost as xgb
 
-MODEL_ID = "f6da7034a9bd66ab7f34094d760a01c00e9f49e155523b0a2a23c6c8024a9ddc"
-MODEL_TYPE = Model.SHALLOW_NN
-SCALING: ScalingMethod | None = ScalingMethod.STANDARDSCALER
-RESAMPLING: ResamplingMethod | None = None
+MODEL_ID = "170e083b41881e0114732a37b7c7292149589cad5c8a1d26be65b283c0575113"
 
 
 def evaluate(pred: pd.DataFrame, y_true: pd.DataFrame) -> tuple[dict, pd.DataFrame]:
@@ -52,16 +46,24 @@ def get_classification_results(_cm: pd.DataFrame) -> tuple[int, int, int, int]:
 
 
 if __name__ == "__main__":
-    export_service = ExportService(collection="federated_fine_tuned", database="paper_2")
+    arg_service = ArgumentService(model=True, resampling=True, scaling=True, database=True, features=True, dataset=True)
+    model_enum = arg_service.get_models()[0]
+    resampling_method = arg_service.get_resampling_methods()[0]
+    scaling_method = arg_service.get_scaling_methods()[0]
+    database = arg_service.get_database()
+    with_features = arg_service.get_features()[0]
+    dataset = arg_service.get_dataset()
+
+    export_service = ExportService(collection="federated_fine_tuned", database=database)
 
     root_path = Path(__file__).parent.parent.parent.parent
     run_info = {
         "_id": MODEL_ID,
-        "model": MODEL_TYPE.value,
+        "model": model_enum.value,
         "pre-processing": {
-            "features": False,
-            "resampling": {"method": RESAMPLING.value if RESAMPLING is not None else None},
-            "scaling": {"method": SCALING.value},
+            "features": with_features,
+            "resampling": {"method": resampling_method.value if resampling_method is not None else None},
+            "scaling": {"method": scaling_method.value if scaling_method is not None else None},
         },
         "participants": [],
     }
@@ -70,29 +72,30 @@ if __name__ == "__main__":
     for idx, participant in enumerate(utils.get_list_of_participants(dataset=Dataset.NURSE)):
         run_info["participants"].append({"participant": participant})
 
-        df = pd.read_pickle(root_path / "datasets" / "nurse" / "paper" / f"{participant}.pkl")
-        x = df.drop(columns=["Label", "Participant"])
-        y = df["Label"]
+        df = utils.load_data(dataset=dataset, with_features=with_features, which=participant)
 
-        x_train, x_test, y_train, y_test = train_test_split(
-            x, y, test_size=0.2, random_state=42, shuffle=True, stratify=y
+        x_train, x_val, x_test, y_train, y_val, y_test = utils.split_data(
+            df=df, with_features=with_features, model=model_enum
         )
 
-        # scale
-        if SCALING is not None:
-            scaler = StandardScaler()
-            x_train = scaler.fit_transform(x_train)
-            x_test = scaler.transform(x_test)
+        scaler = utils.get_scaler(method=scaling_method)
+        resampler = utils.get_resampler(method=resampling_method)
 
-        if MODEL_TYPE == Model.XGBOOST:
+        # scale
+        if scaler is not None:
+            scaler.fit(x_train)
+            x_train = scaler.transform(x_train)
+            x_val = scaler.transform(x_val) if x_val is not None else None
+            x_test = scaler.transform(x_test)
+        # resample
+        if resampler is not None:
+            x_train, y_train = resampler.fit_resample(x_train, y_train)
+
+        if model_enum == Model.XGBOOST:
             # Load the model
             model = xgb.Booster()
-            model.load_model(
-                root_path / "results" / "bfh_server" / "federated" / "models" / f"{MODEL_ID}_run_index_0_model.json"
-            )
-            params = joblib.load(
-                root_path / "results" / "bfh_server" / "federated" / "models" / f"{MODEL_ID}_run_index_0_params.pkl"
-            )
+            model.load_model(root_path / "results" / "federated" / "models" / f"{MODEL_ID}_run_index_0_model.json")
+            params = joblib.load(root_path / "results" / "federated" / "models" / f"{MODEL_ID}_run_index_0_params.pkl")
 
             # Local training
             bst = xgb.train(params, xgb.DMatrix(x_train, label=y_train), num_boost_round=100, xgb_model=model)
@@ -100,11 +103,10 @@ if __name__ == "__main__":
             y_pred = bst.predict(xgb.DMatrix(x_test, label=y_test))
             # Convert predictions to binary labels
             y_pred = (y_pred > 0.5).astype(int)
-        else:
+        elif model_enum == Model.SHALLOW_NN:
             model = joblib.load(
                 Path(__file__).parent.parent.parent.parent
                 / "results"
-                / "bfh_server"
                 / "federated"
                 / "models"
                 / f"{MODEL_ID}_run_index_0_model.pkl"
@@ -117,12 +119,31 @@ if __name__ == "__main__":
                 metrics=["accuracy"],
             )
             # Fine-tune the model
-            history = model.fit(x_train, y_train, epochs=5, batch_size=128, validation_split=0.2)
+            history = model.fit(
+                x_train, y_train, epochs=5, batch_size=128, validation_split=0.2, validation_data=(x_val, y_val)
+            )
 
             # sklearn metrics: accuracy, f1, precision, recall
             y_pred = model.predict(x_test)
             # Convert predictions to binary labels
             y_pred = (y_pred > 0.5).astype(int)
+        elif model_enum == Model.LOGISTIC_REGRESSION:
+            model = joblib.load(
+                Path(__file__).parent.parent.parent.parent
+                / "results"
+                / "federated"
+                / "models"
+                / f"{MODEL_ID}_run_index_0_model.pkl"
+            )
+
+            model.fit(x_train, y_train)
+
+            # sklearn metrics: accuracy, f1, precision, recall
+            y_pred = model.predict(x_test)
+            # Convert predictions to binary labels
+            y_pred = (y_pred > 0.5).astype(int)
+        else:
+            raise Exception(f"Could not initialize model {model_enum.value} for config")
 
         scores_test, cm = evaluate(y_true=y_test, pred=y_pred)
         scores = {"testing_set": scores_test}
